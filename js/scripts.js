26dd6eb96ce1869573e3c43aeba8188a4ac65e7e
Katrina Hirsch <katrina.hirsch@gmail.com>
2016-08-21 12:47:07 -0700

    fix business logic display

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,38 @@
 // Business Logic
-var numbers = [];
-var fifteens = [15, 30, 45, 60, 75, 90]
-var fives = [5, 10, 20, 25, 35, 40, 50, 55, 65, 70, 80, 85, 95, 100]
-var threes = [3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39, 42, 48, 51, 54, 57, 63, 66, 69, 72, 78, 81, 84, 87, 93, 96, 99]
+var pingPong = function(input) {
 
-var pingPong = function(countTo) {
-  for (var i = 1; i <= countTo; i++) {
-    numbers.push(i + "");
+  var output = "";
+  for (var i = 1; i <= input; i++) {
+
+    if (i % 15 === 0){
+      output += "ping-pong" + " ";
+    } else if (i % 5 === 0) {
+      output += "pong" + " ";
+    } else if (i % 3 === 0) {
+      output += "ping" + " ";
+    } else {
+      output += i + " ";
+    }
   }
 
-  // var newNumbers = numbers.join(", ");
-  //
-  // if (countTo % 3 == 0) {
-  //   for (var index = 0; index < newNumbers.length; index += 1) {
-  //     var removeNumbers = threes[index];
-  //
-  //     for (var index2 = 0; index2 < newNumbers.length; index2 += 1) {
-  //       newNumbers = newNumbers.replace(removeNumbers, "ping");
-  //     }
-  //   };
-  //
-  // } else if (countTo % 5 == 0) {
-  //   for (var index = 0; index < newNumbers.length; index += 1) {
-  //     var removeNumbers = fives[index];
-  //
-  //     for (var index2 = 0; index2 < newNumbers.length; index2 += 1) {
-  //       newNumbers = newNumbers.replace(removeNumbers, "pong");
-  //     }
-  //   };
-  //
-  // } else if (countTo % 15 == 0) {
-  //   for (var index = 0; index < newNumbers.length; index += 1) {
-  //     var removeNumbers = fifteens[index];
-  //
-  //     for (var index2 = 0; index2 < newNumbers.length; index2 += 1) {
-  //       newNumbers = newNumbers.replace(removeNumbers, "ping-pong");
-  //     }
-  //   };
-  //
-  // } else {
-  //   console.log("meow");
-  // }
+  return output;
 
 };
 
 // UI Logic
 $(document).ready(function(){
+
   $("#input form").submit(function(event){
     event.preventDefault();
     var countTo = parseInt($("input#number").val());
-    var output = pingPong(countTo);
+    var output = pingPong(countTo)
 
-    $("#ping-pong").text(numbers.join(", "));
-    $("#output").show();
+    if (!countTo) {
+      alert("You must enter a number to play.")
+    } else {
+      $("#ping-pong").append(output);
+      $("#output").show();
+    }
   });
 
   $("#reset").click(function(event) {
